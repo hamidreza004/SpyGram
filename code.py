1936aa3ef91555ccb7ad748434bbad54462035f0
sadegh <smahdavi4@gmail.com>
2017-04-15 21:32:28 +0430

    added username search

--- code.py
+++ code.py
@@ -2,41 +2,53 @@ from pytg.utils import coroutine
 import datetime 
 from pytg import Telegram
 import time
+import json
 
-tg = Telegram(telegram="tg/bin/telegram-cli",pubkey_file="tg/tg-server.pub")
+tg = Telegram(telegram="../tg/bin/telegram-cli",pubkey_file="../tg/tg-server.pub")
 receiver = tg.receiver
 sender = tg.sender
 sender.dialog_list()
 sender.contacts_list()
 
-MAX = 10
-
+MAX = 3
+js = {}
 
 def getlist ():
 	arr = []
 	F = open("names.txt",'r')
 	data = F.readlines()
-	for x in data : 
-		arr.append(x)
+	for x in data :
+                if x[0] == '@':                        
+                        arr.append(sender.contacts_search(unicode(x[1:]))['print_name'])
+                else :
+                        arr.append(x[0:(len(x)-1)])
 	return arr
-def show_list(lst):
+def make_js(lst):
 	for TimE in range(MAX):
 		time.sleep(1)
+		js[str(TimE)] = {}
 		now = datetime.datetime.now()
-		print "In Time " , str(TimE) 
+		#print "In Time " , str(TimE) 
 		for x in lst :
 			user = sender.user_info(unicode(x))
 			try:
 				when = user[u'when'][0:19]
 				if(str(when) > str(now)):
-					print "    user" , x , " is online"
+                                        js[str(TimE)][x] = "online"
+					#print "    user" , x , " is online"
 				else :
-					print "    user" , x , " is offline"
+                                        js[str(TimE)][x] = "offline"
+					#print "    user" , x , " is offline"
 			except :
 				pass
 
 
-show_list(getlist())
+make_js(getlist())
+print json.dumps(js)
+#print sender.contacts_search(u'Yousef1561')
+
+#print sender.user_info(sender.contacts_search(u'Yousef1561')['print_name'])
+
 
 
 
